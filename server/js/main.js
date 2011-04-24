5a679caa1e0b1c1cdfabd762f7495326d8f4d835
David Illsley <david@illsley.org>
2011-04-24 15:21:24 +0100

    Add license headers

--- server/js/main.js
+++ server/js/main.js
@@ -1,4 +1,18 @@
-// N.B. Any re-use of this code is subject to acceptance of the TFL data use agreement (and of acceptance of your planned useage by TFL).
+/*
+   Copyright 2011 David Illsley <david@illsley.org>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 var http = require('http');
 
@@ -16,5 +30,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(JSON.stringify(currentStatus));
 }).listen(1337, "127.0.0.1");
-//console.log('Server running at http://127.0.0.1:1337/');
 

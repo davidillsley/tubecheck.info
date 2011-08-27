fe1897f75e17fa73db8d2229b72ea5a3ccf19dba
David Illsley <david@illsley.org>
2011-08-27 21:36:30 +0100

    Fix 'more details'

--- client/data/panel.js
+++ client/data/panel.js
@@ -1,3 +1,93 @@
+var lineInfo =[
+{
+"id":"1",
+"lineName":"Bakerloo",
+"background":"#AE6118"
+},
+{
+"id":"2",
+"lineName":"Central",
+"background":"#E41F1F"
+},
+{
+"id":"3",
+"lineName":"Victoria",
+"background":"#009FE0"
+},
+{
+"id":"4",
+"lineName":"Jubilee",
+"background":"#8F989E"
+},
+{
+"id":"5",
+"lineName":"Northern",
+"background":"#000000"
+},
+{
+"id":"6",
+"lineName":"Piccadilly",
+"background":"#0450A1"
+},
+{
+"id":"7",
+"lineName":"Circle",
+"background":"#F8D42D"
+},
+{
+"id":"8",
+"lineName":"Hammersmith and City",
+"background":"#E899A8"
+},
+{
+"id":"9",
+"lineName":"District",
+"background":"#00A575"
+},
+{
+"id":"11",
+"lineName":"Metropolitan",
+"background":"#893267"
+},
+{
+"id":"12",
+"lineName":"Waterloo and City",
+"background":"#70C3CE"
+},
+{
+"id":"81",
+"lineName":"DLR",
+"background":"#00BBB4"
+},
+{
+"id":"82",
+"lineName":"Overground",
+"background":"#F86C00"
+}
+];
+var lineMap = {};
+var tableRef = document.getElementById("summary");
+for(var i=0;i<lineInfo.length;i++){
+	var newRow   = tableRef.insertRow(i);
+
+	var lineCell  = newRow.insertCell(0);
+	lineCell.id = "line_"+lineInfo[i].id;
+	lineCell.style.background = lineInfo[i].background;
+	lineCell.textContent = lineInfo[i].lineName;
+	lineCell.className = "line";
+
+	var statusCell = newRow.insertCell(1);
+	statusCell.id =  "line_"+lineInfo[i].id+"_status";
+	statusCell.className = "ss";
+	statusCell.textContent = "Loading...";
+
+	var moreCell = newRow.insertCell(2);
+	moreCell.id = "line_"+lineInfo[i].id+"_more";
+	moreCell.className = "more";
+	moreCell.textContent = "More...";
+      	lineMap[lineInfo[i].id] = lineInfo[i];
+}
+
 function showMore(lineStatus){
 	var self = this;
 	this.lineStatus = lineStatus;

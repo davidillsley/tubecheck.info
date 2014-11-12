0aebefdf5f85020c8dd49e8931b556b5dd27f7c6
David Illsley <david@illsley.org>
2014-11-12 21:02:41 +0000

    Bump to use sdk/ui for ToggleButton

--- client/lib/main.js
+++ client/lib/main.js
@@ -5,6 +5,26 @@ const data = require("sdk/self").data;
 var latestResponse = {};
 var latestETag = "*";
 
+
+var { ToggleButton } = require('sdk/ui/button/toggle');
+var panels = require("sdk/panel");
+var self = require("sdk/self");
+
+var button = ToggleButton({
+  id: "my-button",
+  label: "my button",
+  icon: "./tube.png",
+  onChange: handleChange
+});
+
+function handleChange(state) {
+  if (state.checked) {
+    panel.show({
+      position: button
+    });
+  }
+}
+
 var panel = require("sdk/panel").Panel({
   contentScriptFile: data.url("panel.js"),
   contentScriptWhen: "ready",
@@ -20,11 +40,13 @@ function doRequest(){
   Request({
     url: "http://tubecheck.info/status",
     headers: {"If-None-Match":latestETag},
+    overrideMimeType: "application/json; charset=utf-8",
     onComplete: processResponse 
   }).get();	
 }
 
 function showLoading(){
+  button.state('window', {checked: false});
   var message = {"type":"loading"};
   panel.port.emit("loading",message);
 }
@@ -38,10 +60,3 @@ function processResponse(response){
   message.details  = latestResponse;
   panel.port.emit("details",message)
 }
-
-var widget = widgets.Widget({
-  id:"tubecheck.info/widget1",
-  label: "Tube Status",
-  contentURL: data.url("tube.png"),
-  panel: panel
-});
